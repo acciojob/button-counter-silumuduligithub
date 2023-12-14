e042e24709c0ec28e3751da97c9dfa34ead0c5b0
silulitgithub <kumarsilu530@gmail.com>
2023-12-14 23:01:24 +0530

    assignment complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let[x, setX] = useState(0);
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Button clicked {x} times</h1>
+        <button onClick={()=>{
+          setX(x + 1);
+        }}>Click me</button>
     </div>
   )
 }
